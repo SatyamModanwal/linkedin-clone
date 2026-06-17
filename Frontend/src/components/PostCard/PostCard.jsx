715c469f764f7be91163bb77c8efa4ac97e277fc
sattu175 <satyammodanwal815@gmail.com>
2026-06-17 11:24:04 +0530

    Updated Postcard component

--- Frontend/src/components/PostCard/PostCard.jsx
+++ Frontend/src/components/PostCard/PostCard.jsx
@@ -6,46 +6,70 @@
 
 
 //s-26
-function PostCard() {
-  return (
-    <div className="bg-white p-4 rounded-lg shadow mt-4">
+// function PostCard() {
+//   return (
+//     <div className="bg-white p-4 rounded-lg shadow mt-4">
 
-      <div className="flex items-center gap-3">
+//       <div className="flex items-center gap-3">
 
-        <img
-          src="https://via.placeholder.com/50"
-          alt=""
-          className="rounded-full"
-        />
+//         <img
+//           src="https://via.placeholder.com/50"
+//           alt=""
+//           className="rounded-full"
+//         />
 
-        <div>
-          <h3 className="font-bold">
-            John Doe
-          </h3>
+//         <div>
+//           <h3 className="font-bold">
+//             John Doe
+//           </h3>
 
-          <p className="text-gray-500 text-sm">
-            MERN Stack Developer
-          </p>
-        </div>
+//           <p className="text-gray-500 text-sm">
+//             MERN Stack Developer
+//           </p>
+//         </div>
 
-      </div>
+//       </div>
 
-      <p className="mt-4">
-        Excited to share my new LinkedIn Clone project built using MERN Stack 🚀
-      </p>
+//       <p className="mt-4">
+//         Excited to share my new LinkedIn Clone project built using MERN Stack 🚀
+//       </p>
 
-      <div className="flex justify-around mt-4 border-t pt-3">
+//       <div className="flex justify-around mt-4 border-t pt-3">
 
-        <button>👍 Like</button>
+//         <button>👍 Like</button>
 
-        <button>💬 Comment</button>
+//         <button>💬 Comment</button>
 
-        <button>↗ Share</button>
+//         <button>↗ Share</button>
 
-      </div>
+//       </div>
 
-    </div>
-  );
-}
+//     </div>
+//   );
+// }
 
-export default PostCard;
+// export default PostCard;
+
+//s-28.3
+const posts = [
+  {
+    id: 1,
+    name: "John Doe",
+    role: "MERN Stack Developer",
+    content: "Excited to share my LinkedIn Clone Project 🚀",
+  },
+
+  {
+    id: 2,
+    name: "Rahul Sharma",
+    role: "Frontend Developer",
+    content: "Learning React and Tailwind CSS 🔥",
+  },
+
+  {
+    id: 3,
+    name: "Aman Kumar",
+    role: "Full Stack Developer",
+    content: "Working on LinkedIn Clone using MERN Stack 💻",
+  },
+];
